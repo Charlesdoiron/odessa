20e8564f47894644a5ea1ae08d5bc088deb254d9
charlesdoiron <doironcharles@gmail.com>
2022-03-24 10:29:41 +0100

    Add Boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "redux/store/history";
 
-function App() {
+// Pages
+import { Home } from "page/home";
+
+// Styles
+import "./App.css";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Home />
+      </ConnectedRouter>
+    </Provider>
   );
-}
+};
 
 export default App;
