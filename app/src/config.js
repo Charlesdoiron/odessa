b08608645f7ef10611433162b61a6ba930e8b344
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-03-25 10:35:44 +0100

    fix api url

--- app/src/config.js
+++ app/src/config.js
@@ -1,6 +1,3 @@
-const API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://api.odessa.cleverapps.io"
-    : "http://localhost:3615";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3615";
 
 export { API_URL };
