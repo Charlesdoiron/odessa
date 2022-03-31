b3d76f1792d195da7ccd8d526a41d2cc9160569d
charlesdoiron <doironcharles@gmail.com>
2022-03-31 19:23:08 +0200

    wip params

--- app/src/components/filters/subFilters/index.tsx
+++ app/src/components/filters/subFilters/index.tsx
@@ -49,7 +49,10 @@ export const SubFilters: React.FC<Props> = ({ parentFilter }) => {
   const [selected, setSelected] = useState<string[]>([]);
 
   const handleClick = useCallback(
+    // TODO: Don't delete the location params
     (value: string) => {
+      const currentLocation = searchParams.get("location");
+
       let newFilter = [];
       if (selected.includes(value)) {
         newFilter = selected.filter((item) => item !== value);
@@ -61,41 +64,61 @@ export const SubFilters: React.FC<Props> = ({ parentFilter }) => {
       for (const [key, param] of searchParams.entries()) {
         searchParamsObject[key] = param;
       }
-      setSearchParams({ ...searchParamsObject, extra: newFilter.join(",") });
+      setSearchParams({
+        ...searchParamsObject,
+        extra: newFilter.join(","),
+        location: currentLocation,
+      });
     },
     [searchParams, selected, setSearchParams]
   );
 
+  const handleDelete = () => {
+    // TODO: Don't delete the location params
+    // const currentLocation = searchParams.get("location");
+    // console.log("current", currentLocation);
+    // if (currentLocation) {
+    //   const loc = { location: currentLocation };
+    //   setSearchParams(loc);
+    // }
+    setSearchParams({});
+    setSelected([]);
+  };
+
+  if (data.length === 0) return <></>;
   return (
-    <div className="grid grid-cols-3 gap-8 items-center">
-      <div className="col-span-2">
-        <nav className="flex space-x-4">
-          {data.map(({ label, value }) => {
-            const isActive = selected.includes(value);
-            return (
-              <button
-                onClick={() => handleClick(value)}
-                key={value}
-                className={classNames(
-                  isActive ? "bg-indigo-900  border-transparent text-white" : "bg-transparent",
-                  "text-white text-sm  rounded-md  border  px-3 py-2   hover:text-indigo-200 focus:outline-none "
-                )}>
-                {label}
-              </button>
-            );
-          })}
-          {Boolean(selected[0]) && (
+    <div>
+      <nav className="flex space-x-4 items-center">
+        <p className="text-indigo-900">|</p>
+        {data.map(({ label, value }) => {
+          const isActive = selected.includes(value);
+          return (
             <button
-              onClick={() => setSelected([])}
-              key="delete"
+              onClick={() => handleClick(value)}
+              key={value}
               className={classNames(
-                "text-white text-sm   underline  hover:text-indigo-200 focus:outline-none "
-              )}>
-              Effacer la selection
+                isActive
+                  ? "bg-indigo-900  border-transparent text-white"
+                  : "bg-transparent",
+                "text-white text-sm  rounded-md  border  px-3 py-2   hover:text-indigo-200 focus:outline-none "
+              )}
+            >
+              {label}
             </button>
-          )}
-        </nav>
-      </div>
+          );
+        })}
+        {Boolean(selected[0]) && (
+          <button
+            onClick={() => handleDelete()}
+            key="delete"
+            className={classNames(
+              "text-white text-sm   underline  hover:text-indigo-200 focus:outline-none "
+            )}
+          >
+            Effacer la selection
+          </button>
+        )}
+      </nav>
     </div>
   );
 };
